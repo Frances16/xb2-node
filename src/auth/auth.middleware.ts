d8f6756eb369aa5ad890a29377914d3b820d1a8a
cc <136565456@qq.com>
2020-07-20 22:24:58 +0800

    authentication

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 import * as userService from '../user/user.service';
 import bcryptjs from 'bcryptjs';
+import { PUBLIC_KEY } from '../app/app.config';
+import { TokenPayload } from './auth.interface';
 
+/* 验证用户登录数据 */
 export const validateLoginData = async (
   request: Request,
   response: Response,
@@ -20,5 +24,31 @@ export const validateLoginData = async (
   const matched = await bcryptjs.compare(password, user.password);
   if (!matched) return next(new Error('password not match'));
 
+  request.body.user = user;
+
   next();
+};
+
+/* 验证用户身份 */
+export const authGuard = (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  console.log('验证用户身份');
+  try {
+    const authorization = request.header('Authorization');
+    if (!authorization) throw new Error();
+
+    const token = authorization.replace('Bearer ', '');
+    if (!token) throw new Error();
+
+    const decoded = jwt.verify(token, PUBLIC_KEY, { algorithms: ['RS256'] });
+
+    request.user = decoded as TokenPayload;
+
+    next();
+  } catch (error) {
+    next(new Error('UNAUTHORIZED'));
+  }
 };
